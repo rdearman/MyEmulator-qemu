ea59d32fea190351d64a16c0fb051614118c5a41
Rick Dearman <rick@rdearman.org>
2026-09-20 14:30:39 +0100

    Add initial musl ABI headers

--- toolchain/musl/myemulator2/reloc.h
+++ toolchain/musl/myemulator2/reloc.h
@@ -1,6 +1,10 @@
 #define LDSO_ARCH "myemulator2"
 #define TPOFF_K 0
 
+/* Values from the MyEmulator2 ELF psABI; musl's generic loader only needs
+ * the absolute/relative word relocation during the first static port. */
+#define R_MYEMULATOR2_32 1
+
 #define REL_SYMBOLIC R_MYEMULATOR2_32
 #define REL_RELATIVE R_MYEMULATOR2_32
 
